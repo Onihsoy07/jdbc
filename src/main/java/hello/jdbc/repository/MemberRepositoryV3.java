7f821ca952e99bbbdcb7c082ccea6e46f6f9be8e
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-07-22 23:08:39 +0900

    DataSourceTransactionManager로 트랜잭션 적용

---
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV3.java
@@ -0,0 +1,125 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class MemberRepositoryV3 {
+
+    private final DataSource dataSource;
+
+    public Member save(Member member) throws SQLException {
+        String sql = "insert into member(member_id, money) values (?, ?)";
+
+        Connection conn = null;
+        PreparedStatement pst = null;
+
+        try {
+            conn = getConnection();
+            pst = conn.prepareStatement(sql);
+            pst.setString(1, member.getMemberId());
+            pst.setInt(2, member.getMoney());
+            pst.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(conn, pst, null);
+        }
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, memberId);
+
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                Member member = Member.builder()
+                        .memberId(rs.getString("member_id"))
+                        .money(rs.getInt("money"))
+                        .build();
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId = " + memberId);
+            }
+        } catch (SQLException e) {
+            log.info("db error", e);
+            throw e;
+        } finally {
+            close(conn, ps, rs);
+        }
+    }
+
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money=? where member_id=?";
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+
+        try {
+            conn = getConnection();
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, money);
+            ps.setString(2, memberId);
+            int resultSize = ps.executeUpdate();
+            log.info("resultSize={}", resultSize);
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(conn, ps, null);
+        }
+    }
+
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id=?";
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+
+        try {
+            conn = getConnection();
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, memberId);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(conn, ps, null);
+        }
+    }
+
+    //Statement의 Exception이 터져도 Connection close 됨
+    private void close(Connection conn, Statement st, ResultSet resultSet) {
+        JdbcUtils.closeResultSet(resultSet);
+        JdbcUtils.closeStatement(st);
+        //트랜잭션 동기화 사용하려면 DataSourceUtils 사용
+        //close 대용 DataSourceUtils.releaseConnection 동기화된 Connection은 close 안됨
+        DataSourceUtils.releaseConnection(conn, dataSource);
+    }
+
+    private Connection getConnection() throws SQLException {
+        //트랜잭션 동기화 사용하려면 DataSourceUtils 사용
+        Connection con = DataSourceUtils.getConnection(dataSource);
+        log.info("get connection={}, class", con, con.getClass());
+        return con;
+    }
+}
