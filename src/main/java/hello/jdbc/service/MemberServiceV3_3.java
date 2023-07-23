406b8c992f0e255e4fb81055bf9c12c89ee5c50c
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-07-23 17:29:32 +0900

    @Transactional 사용

--- src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -5,34 +5,22 @@ import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 @Slf4j
-public class MemberServiceV3_2 {
+@RequiredArgsConstructor
+public class MemberServiceV3_3 {
 
-    private final TransactionTemplate transactionTemplate;
     private final MemberRepositoryV3 memberRepository;
 
-    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
-        this.transactionTemplate = new TransactionTemplate(transactionManager);
-        this.memberRepository = memberRepository;
-    }
-
+    @Transactional
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-
-        transactionTemplate.executeWithoutResult((status) -> {
-            try {
-                //비즈니스 로직
-                bizLogic(fromId, toId, money);
-            } catch (SQLException e) {
-                throw new IllegalStateException(e);
-            }
-        });
+        //비즈니스 로직
+        bizLogic(fromId, toId, money);
     }
 
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
@@ -43,7 +31,6 @@ public class MemberServiceV3_2 {
         validation(toMember);
         memberRepository.update(toId, toMember.getMoney() + money);
     }
-
     private void validation(Member toMember) {
         if (toMember.getMemberId().equals("ex")) {
             throw new IllegalStateException("이체 에러");
