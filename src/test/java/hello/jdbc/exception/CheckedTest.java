a16f8d8f6cbd5bbfca01b390bf2fcea087d6b081
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-07-24 23:00:18 +0900

    체크 예외 처리 확인

---
+++ src/test/java/hello/jdbc/exception/CheckedTest.java
@@ -0,0 +1,57 @@
+package hello.jdbc.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+public class CheckedTest {
+
+    @Test
+    void checked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void checked_throw() throws MyCheckedException {
+        Service service = new Service();
+        assertThatThrownBy(() -> {
+            service.callThrow();
+        }).isInstanceOf(Exception.class);
+        assertThat(Exception.class.isAssignableFrom(MyCheckedException.class)).isTrue();
+    }
+
+    static class MyCheckedException extends Exception {
+        public MyCheckedException(String message) {
+            super(message);
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+
+        public void callCatch() {
+            try {
+                repository.call();
+            } catch (MyCheckedException e) {
+                log.info("예외 처리, message={}", e.getMessage(), e);
+            }
+        }
+
+        public void callThrow() throws MyCheckedException {
+            repository.call();
+        }
+    }
+
+    static class Repository {
+        public void call() throws MyCheckedException {
+            throw new MyCheckedException("Ex");
+        }
+
+    }
+
+
+}
