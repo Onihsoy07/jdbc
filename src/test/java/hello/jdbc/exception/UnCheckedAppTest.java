2887ae18c728e6af0e7226362ced3644cf6894c0
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-07-25 23:38:47 +0900

    체크 예외 -> 언체크 예외 변환 사용

---
+++ src/test/java/hello/jdbc/exception/UnCheckedAppTest.java
@@ -0,0 +1,74 @@
+package hello.jdbc.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+public class UnCheckedAppTest {
+
+    @Test
+    void unchecked() {
+        Controller controller = new Controller();
+        assertThatThrownBy(() -> {
+            controller.request();
+        }).isInstanceOf(Exception.class);
+    }
+
+    static class Controller {
+        Service service = new Service();
+
+        public void request() throws SQLException, ConnectException {
+            service.logic();
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+
+        public void logic() throws SQLException, ConnectException {
+            repository.call();
+            networkClient.call();
+        }
+    }
+
+    static class Repository {
+        public void call() {
+            try {
+                runSQL();
+            } catch (SQLException e) {
+                throw new RuntimeSQLException(e);
+            }
+        }
+
+        public void runSQL() throws SQLException {
+            throw new SQLException("ex");
+        }
+    }
+
+    static class NetworkClient {
+        public void call() {
+            throw new RuntimeConnectionException("연결 실패");
+        }
+    }
+
+    static class RuntimeConnectionException extends RuntimeException {
+        public RuntimeConnectionException(String message) {
+            super(message);
+        }
+    }
+
+    static class RuntimeSQLException extends RuntimeException {
+        public RuntimeSQLException(Throwable cause) {
+            super(cause);
+        }
+    }
+
+
+
+}
